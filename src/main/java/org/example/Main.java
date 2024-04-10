5729e3772248082b96d5e3ee972deba0b98bd3e0
AlexgorAlexgor <alexgor@gmx.de>
2024-04-10 23:45:06 +0200

    Translator

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main {
+ //   public static void main(String[] args) {
+  //      System.out.println("Hello world!");
+  //  }
+}
+//import com.google.cloud.translate.Translate;
+//import com.google.cloud.translate.TranslateOptions;
+//import com.google.cloud.translate.Translation;
+//
+//public class Translator {
+//    public static void main(String[] args) {
+//        // Set up Google Cloud Translation client
+//        Translate translate = TranslateOptions.getDefaultInstance().getService();
+//
+//        // Text to translate
+//        String text = "Hello, how are you?";
+//
+//        // Translate text to German
+//        String targetLanguage = "de"; // "de" for German
+//        Translation translation = translate.translate(text, Translate.TranslateOption.targetLanguage(targetLanguage));
+//
+//        // Display translated text
+//        System.out.println("Original: " + text);
+//        System.out.println("Translation: " + translation.getTranslatedText());
+//    }
+//}
